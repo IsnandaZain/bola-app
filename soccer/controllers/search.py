0dc3ea330d60610f4d59ba86a944d1ac4c060ab8
isnandazain <bitartikel@gmail.com>
2019-12-13 09:06:55 +0700

    update search team and add search player

--- soccer/controllers/search.py
+++ soccer/controllers/search.py
@@ -2,16 +2,18 @@ from flask_sqlalchemy import Pagination
 
 from string import punctuation
 
-from soccer.models import db, Team
+from soccer.models import db, Team, Player
 
 
-def search_team(keyword: str, page: int = 1, count: int = 12) -> Pagination:
+def search_team(keyword: str, page: int = 1, count: int = 12, next_id: int = 0, last_id: int = 0) -> Pagination:
     """Search teams
 
     Args:
         keyword: keyword for search
         page: number of page
         count: item per page
+        next_id: next id team yang dicari
+        last_id: last id team yang dicari
     
     Returns:
         Pagination
@@ -57,3 +59,59 @@ def search_team(keyword: str, page: int = 1, count: int = 12) -> Pagination:
     )
 
     return teams
+
+
+def search_player(keyword: str, page: int = 1, count: int = 12, next_id: int = 0, last_id: int = 0) -> Pagination:
+    """Search players
+
+    Args:
+        keyword: keyword players
+        page: number of page
+        count: item per page
+        next_id: next id player yang dicari
+        last_id: last id player yang dicari
+
+    Returns:
+        Pagination
+    """
+    keyword = keyword.strip(punctuation)
+    if keyword == "":
+        return None
+
+    filter_apply = [Player.fullname.like("%{}%".format(keyword))]
+
+    if bool(next_id):
+        filter_apply.append(Player.id < next_id)
+
+    if bool(last_id):
+        filter_apply.append(Player.id > last_id)
+
+    # generate query sort
+    sort_collections = {
+        "-name": Player.name.desc(),
+        "name": Player.name.asc(),
+        "-id": Player.id.desc(),
+        "id": Player.id.asc(),
+        "match": db.case(
+            (
+                (Player.name == keyword, 0),
+                (Player.name.like("{}%".format(keyword)), 1)
+                (Player.name.like("%{}%".format(keyword)), 2)
+                (Player.name.like("%{}".format(keyword)), 3)
+            ), else_=4
+        ).asc()
+    }
+
+    sort_apply = sort_collections[sort]
+
+    players = Player.query.filter(
+        *filter_apply
+    ).order_by(
+        sort_apply
+    ).paginate(
+        page=page,
+        per_page=count,
+        error_out=False
+    )
+
+    return players
--- soccer/routes/search.py
+++ soccer/routes/search.py
@@ -5,13 +5,14 @@ from soccer.exceptions import BadRequest
 from soccer.libs.ratelimit import ratelimit
 
 from soccer.models import team as team_mdl
+from soccer.models import player as player_mdl
 
 bp = Blueprint(__name__, 'search')
 
 
 @bp.route("/search/team")
 def search_team():
-    """Search user
+    """Search team
 
     **endpoint**
 
@@ -72,15 +73,15 @@ def search_team():
     page = request.args.get("page", "1")
     count = request.args.get("count", "12")
     sort = request.args.get("sort", "match")
-    next_id = request.args.get("next_id", "next_id")
-    last_id = request.args.get("last_id", "last_id")
+    next_id = request.args.get("next_id", "0")
+    last_id = request.args.get("last_id", "0")
 
     # raise BadRequest is missing keyword
     if not keyword:
         raise BadRequest("Keyword kosong")
 
     if not page.isdigit() or not count.isdigit():
-        raise BadRequest("Page dan count harus integer")
+        raise BadRequest("page dan count harus integer")
 
     if sort and sort not in ("-id", "id", "match", "-name", "name"):
         raise BadRequest("Sort tidak didukung")
@@ -93,7 +94,7 @@ def search_team():
     
     teams = search.search_team(keyword, next_id, last_id, page, count, sort)
 
-    # get users result
+    # get teams result
     result = []
     if teams != None:
         for team in teams.items:
@@ -123,6 +124,91 @@ def search_team():
         "has_prev": teams.has_prev if teams else False,
         "has_next": teams.has_next if teams else False,
         "total": teams.total if teams else 0
+    }
+
+    return jsonify(response)
+
+
+@bp.route("/search/player")
+def search_player():
+    """Search player
+
+    **endpoint**
+
+    .. sourcecode:: http
+
+        GET /search/player
+
+    **success response**
+
+    .. sourcecode:: http
+
+        HTTP/1.1 200 OK
+        Content-Type: text/javascript
+
+        {
+            "status": 200,
+            "has_next": false,
+            "has_prev": false,
+            "total": 1,
+            "result": [
+                {
+
+                }
+            ]
+        }
+
+    :query keyword: keyword for search
+    :query next_id: start id player for search
+    :query last_id: last id player for search
+    :query page: pagination pag
+    :query count: count result per page
+
+    optional:
+    :query sort: sort list player
+        - match: sort by match query
+        - id: sort by id asc
+        - -id: sort by id desc
+        - name: sort by name asc
+        - -name: sort by name desc
+    """
+    keyword = request.args.get("keyword")
+    page = request.args.get("page", "1")
+    count = request.args.get("count", "12")
+    sort = request.args.get("sort", "match")
+    next_id = request.args.get("next_id")
+    last_id = request.args.get("last_id")
+
+    # raise BadRequest is missing keyword
+    if not keyword:
+        raise BadRequest("Keyword kosong")
+
+    if not page.isdigit() or not count.isdigit():
+        raise BadRequest("page dan count harus integer")
+
+    if sort and sort not in ("-id", "id", "match", "name", "-name"):
+        raise BadRequest("Sort tidak didukung")
+
+    # type conversion
+    page = int(page)
+    count = int(count)
+    next_id = int(next_id)
+    last_id = int(last_id)
+
+    players = search.search_player(keyword, next_id, last_id, page, count, sort)
+
+    # get players result
+    result = []
+    if players != None:
+        for player in players.items:
+
+
+    response = {
+        "status": 200,
+        "result": result,
+        "has_prev": players.has_prev if players else False,
+        "has_next": players.has_next if players else False,
+        "total": players.total if players else 0
     }
 
     return jsonify(response)
